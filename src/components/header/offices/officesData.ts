a9b698904d76e4b99e3a1a920ed7f0b56234c161
Georgii Galechyan <galechyan1991@gmail.com>
2024-05-13 00:18:07 +0400

    Change: officeData.ts and any styles in component Offices.astro

--- src/components/header/offices/officesData.ts
+++ src/components/header/offices/officesData.ts
@@ -8,7 +8,7 @@ const volskaya: IOffice = {
   address: 'ул. Вольская, 11',
   phone: '+7 (917) 302-63-08',
   phoneUrl: 'tel:+79173026308',
-  working: 'Пн-Пт: 08:00-19:00\n  Cб: 10:00-17:00\n  Вс: Выходной',
+  working: 'Пн-Пт: 08:00-19:00\nCб: 10:00-17:00',
   default: true,
 }
 
@@ -18,7 +18,7 @@ const rabochaya: IOffice = {
   address: 'ул. Рабочая, 41/43',
   phone: '+7 (917) 980-94-62',
   phoneUrl: 'tel:+79179809462',
-  working: 'Пн-Пт: 08:00-19:00\n  Cб: 10:00-17:00\n  Вс: Выходной',
+  working: 'Пн-Пт: 08:00-19:00\nCб: 10:00-17:00',
   default: false,
 }
 
