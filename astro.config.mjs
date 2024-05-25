7cb5279260431288e808c3a68b614fe78f487612
Georgii Galechyan <galechyan1991@gmail.com>
2024-05-25 16:07:19 +0400

    Add: Locale 'ru' and 'en'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aladdin-s.ru',
-});
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru', 'en'],
+  },
+})
