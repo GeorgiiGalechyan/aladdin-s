5278670416155a5a2381341ae6ea47229188143d
Georgi Galechian <galechyan1991@gmail.com>
2024-07-20 23:57:53 +0400

    Change: header style (nav, theme select); Add: astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aladdin-s.ru',
   i18n: {
     defaultLocale: 'ru',
-    locales: ['ru', 'en'],
+    locales: ['ru', 'en']
   },
-})
+  integrations: [icon()]
+});
